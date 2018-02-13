9ad02000ee48105b6b4b10339d9f0ffd46160934
derrick-long <long.derrick8@gmail.com>
2018-02-13 10:15:03 -0500

    add look model and api files

---
+++ server/api/look/look.model.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = moongoose.Schema;
+
+var LookSchema = new Schema({
+  image: String,
+  linkURL: String,
+  title: String,
+  description: String,
+  tags: [{
+    type: String
+  }],
+  creator: {
+  type: Schema.ObjectId,
+  ref: 'User'
+  },
+  email: String,
+  userName: String,
+  createTime:{
+    type: Date,
+    'default': Date.now
+  },
+  views: {
+    type: Number,
+    'default': 0
+  },
+  upVotes: {
+    type: Number,
+    'default': 0
+  }
+});
+
+
+
+module.exports = mongoose.model('Look',LookSchema);

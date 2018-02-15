c20311493f5d9d76aa3169a6f1e753febcdc34ae
derrick-long <long.derrick8@gmail.com>
2018-02-15 14:03:18 -0500

    add imgur scraper

---
+++ server/api/imgScraper/scrapers/imgur.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+exports.list = function(url,cb){
+  request(url,function(error,resp,body){
+    if(error){
+      cb({
+        error:error
+      });
+    }
+    if(!error){
+      var $ = cheerio.load(body);
+      var pin = {};
+      var $url = url;
+      var $img = $('.post-image img').attr('src');
+      var $desc = $('.post-image img').attr('alt');
+
+      console.log($img + 'image url');
+
+      var image = {
+        img: "http:" + $img,
+        url: $url,
+        desc: $desc
+      }
+      console.log('scraped:' + image );
+      cb(image);
+    }
+  });
+}

e487bd9fb7e00d90cb1f74ffaa8b5746fe72a8f9
derrick-long <long.derrick8@gmail.com>
2018-02-16 11:19:59 -0500

    add util folder and js file

--- server/api/look/index.js
+++ server/api/look/index.js
@@ -5,4 +5,6 @@ var controller = require('./look.controller');
 var router = express.Router();
 var auth = require('../../auth/auth.service');
 
+router.post('/scrapeUpload',auth.isAuthenticated(), controller.scrapeUpload);
+
 module.exports = router;
---
+++ server/api/look/look.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var _ = require('lodash');
+var Look= require('./look.model');
+var path = require('path');
+var utils = require('../../utils/utils.js');
